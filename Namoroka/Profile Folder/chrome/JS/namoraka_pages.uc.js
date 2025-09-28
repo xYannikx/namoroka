62ff485c7f360a8540290154806f523b7036dac4
aubymori <aubyomori@gmail.com>
2025-09-28 10:14:15 -0500

    Classic about:config page

--- Namoroka/Profile Folder/chrome/JS/namoraka_pages.uc.js
+++ Namoroka/Profile Folder/chrome/JS/namoraka_pages.uc.js
@@ -10,7 +10,8 @@
         "newtab": "chrome://userchrome/content/pages/home/home.xhtml",
         "home": "chrome://userchrome/content/pages/home/home.xhtml",
         "namoroka": "chrome://userchrome/content/windows/options/options.xhtml",
-        "nrabout": "chrome://userchrome/content/windows/aboutDialog/aboutDialog.xhtml"
+        "nrabout": "chrome://userchrome/content/windows/aboutDialog/aboutDialog.xhtml",
+        "config": "chrome://userchrome/content/pages/config/config.xhtml"
     };
     const { AboutPageManager } = ChromeUtils.importESModule("chrome://modules/content/AboutPageManager.sys.mjs");
 
