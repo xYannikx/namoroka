726d98a1e8493cca1a1e1a8386d4940222f977f9
travis <94665268+travy-patty@users.noreply.github.com>
2026-04-11 17:26:20 -0700

    Fix for Firefox 1 Tabs not having selected style

--- Namoroka/Profile Folder/chrome/JS/namoroka_tabscrollbox.uc.js
+++ Namoroka/Profile Folder/chrome/JS/namoroka_tabscrollbox.uc.js
@@ -16,5 +16,38 @@
         link.rel  = "stylesheet";
         link.href = "chrome://userchrome/content/tabscrollbox.uc.css";
         e.shadowRoot.append(link);
+
+        function isVisuallySelected(list) {
+            for (let mut of list) {
+                setTab(mut.target);
+            }
+        }
+        
+        function setTab(mut) {
+            let tab = mut;
+
+            if (tab.nodeName == "tab") {
+                if (tab.hasAttribute("visuallyselected") 
+                    && tab.getAttribute("visuallyselected") !== "true") {
+                    tab.setAttribute("visuallyselected", "true");
+                }
+            }
+        }
+
+        let observer = new MutationObserver(isVisuallySelected);
+        observer.observe(e, { 
+            childList: true,
+            attributes: true,
+            subtree: true,
+            attributeFilter: ["visuallyselected"]
+        });
+
+        let delayedStartupObserver = (aSubject, aTopic, aData) => {
+            if (aSubject == window) {
+                Services.obs.removeObserver(delayedStartupObserver, "browser-delayed-startup-finished");
+                setTab(gBrowser.selectedTab);
+            }
+        };
+        Services.obs.addObserver(delayedStartupObserver, "browser-delayed-startup-finished");
     });
 }
